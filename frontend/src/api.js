772f0b8356d41f8bdafcf2e284d75cd2e6d9b37e
sarveshatawane <sarvesh.atawane@cashfree.com>
2026-03-07 13:39:31 +0530

    pathced big file uploads

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
-const API_BASE = process.env.NODE_ENV === 'production'
-    ? ''
-    : (process.env.REACT_APP_BACKEND_URL || 'http://localhost:8000');
+const API_BASE = process.env.REACT_APP_BACKEND_URL !== undefined ? process.env.REACT_APP_BACKEND_URL : 'http://localhost:8000';
 
 const api = axios.create({
     baseURL: `${API_BASE}/api`,
